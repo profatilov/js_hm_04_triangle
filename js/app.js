796dfd0c836049e2f0a063974e033eb77bee5588
Александр Алексеевич Профатилов <a.profatilov@xado.com>
2022-07-04 22:01:03 +0300

    js optimization

--- js/app.js
+++ js/app.js
@@ -7,57 +7,48 @@
 // .#########.
 // ###########
 
-let resultString = buildChristmasTree(prompt('Height of the tree? (positive or negative integer)')); // positive or negative integer
+let resultString = buildChristmasTree(prompt('Height of the tree? (positive or negative integer)'));
 
 document.querySelector('.result').innerText = resultString;
-console.log(resultString);
+
 
 function buildChristmasTree(rows) {
     rows = parseInt(rows);
 
-    if (!Number.isInteger(rows)) {
+    if (!Number.isInteger(rows) || rows === 0) {
         return 'Param must be positive or negative integer! Thank you for your cooperation!';
     }
 
-    let hashSigns = 0,
-        rowHasHashSigns = {},
-        resultString = "\n",
-        isNegative = Math.sign(rows) === -1;
+    let resultString = '';
+    let isNegative = Math.sign(rows) === -1;
 
     rows = Math.abs(rows);
 
-    for (let row = 1; row <= rows; row++) {
-        if (row === 1) {
-            hashSigns += 1;
-        } else {
-            hashSigns += 2;
-        }
+    for (let row = 0, hashSigns = 0; row < rows; row++) {
+        hashSigns = row === 0 ? hashSigns += 1 : hashSigns += 2;
 
-        rowHasHashSigns[row] = hashSigns;
-    }
-
-    let rowHasHashSignsKeys = Object.keys(rowHasHashSigns);
-    let maxCols = rowHasHashSigns[Object.keys(rowHasHashSignsKeys).length];
-
-    if (isNegative) {
-        rowHasHashSignsKeys = rowHasHashSignsKeys.reverse();
-    }
-
-    rowHasHashSignsKeys.forEach(function(row){
-        let dotsOneSide = (maxCols - rowHasHashSigns[row]) / 2;
+        let rowHashSignsMax = rows * 2 - 1;
+        let dotsOneSide = (rowHashSignsMax - hashSigns) / 2;
         let dotsOneSideString = '';
         let hashSignsString = '';
+        let tempRowString = '';
 
         for (let dot = 0; dot < dotsOneSide; dot++) {
             dotsOneSideString += '.';
         }
 
-        for (let hashSign = 0; hashSign < rowHasHashSigns[row]; hashSign++) {
+        for (let hashSign = 0; hashSign < hashSigns; hashSign++) {
             hashSignsString += '#';
         }
 
-        resultString += dotsOneSideString + hashSignsString + dotsOneSideString + "\n";
-    })
+        tempRowString = dotsOneSideString + hashSignsString + dotsOneSideString + "\n";
+
+        if (isNegative) {
+            resultString = tempRowString.concat(resultString);
+        } else {
+            resultString = resultString.concat(tempRowString);
+        }
+    }
 
     return resultString;
 }
