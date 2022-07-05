48b22d8b7871d96f58792f71665faf0d305a5cdf
Александр Алексеевич Профатилов <a.profatilov@xado.com>
2022-07-05 09:19:18 +0300

    js optimization

--- js/app.js
+++ js/app.js
@@ -13,26 +13,27 @@ document.querySelector('.result').innerText = resultString;
 
 
 function makeTriangle(rows) {
+    let resultStr = '';
+
     rows = parseInt(rows);
 
     if (!Number.isInteger(rows) || rows === 0) {
-        return 'Param must be positive or negative integer! Thank you for your cooperation!';
-    }
+        resultStr = 'Param must be positive or negative integer! Thank you for your cooperation!';
+    } else {
+        let isNegative = Math.sign(rows) === -1;
 
-    let isNegative = Math.sign(rows) === -1;
-    let resultStr = '';
+        rows = Math.abs(rows);
 
-    rows = Math.abs(rows);
+        for (let row = 0, hashSignsCnt = 0; row < rows; row++) {
+            hashSignsCnt = row === 0 ? hashSignsCnt += 1 : hashSignsCnt += 2;
 
-    for (let row = 0, hashSignsCnt = 0; row < rows; row++) {
-        hashSignsCnt = row === 0 ? hashSignsCnt += 1 : hashSignsCnt += 2;
+            let dotsOneSideCnt = ((rows * 2 - 1) - hashSignsCnt) / 2;
+            let dotsOneSideStr = '.'.repeat(dotsOneSideCnt);
+            let hashSignsStr = '#'.repeat(hashSignsCnt);
+            let tempRowStr = dotsOneSideStr + hashSignsStr + dotsOneSideStr + "\n";
 
-        let dotsOneSideCnt = ((rows * 2 - 1) - hashSignsCnt) / 2;
-        let dotsOneSideStr = '.'.repeat(dotsOneSideCnt);
-        let hashSignsStr = '#'.repeat(hashSignsCnt);
-        let tempRowStr = dotsOneSideStr + hashSignsStr + dotsOneSideStr + "\n";
-
-        resultStr = isNegative ? tempRowStr.concat(resultStr) : resultStr.concat(tempRowStr);
+            resultStr = isNegative ? tempRowStr.concat(resultStr) : resultStr.concat(tempRowStr);
+        }
     }
 
     return resultStr;
